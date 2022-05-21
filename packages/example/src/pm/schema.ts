ba729b2cb49445e376646235a20f04cf77f48dc1
湫曗 <autumnsilverleaves@gmail.com>
2022-05-22 02:08:17 +0800

    feat: add pm

---
+++ packages/example/src/pm/schema.ts
@@ -0,0 +1,64 @@
+import { baseKeymap, toggleMark } from 'prosemirror-commands'
+import { redo, undo } from 'prosemirror-history'
+import { keymap } from 'prosemirror-keymap'
+import { Schema } from 'prosemirror-model'
+import { EditorState } from 'prosemirror-state'
+import { history } from 'prosemirror-history'
+
+export const mySchema = new Schema({
+  nodes: {
+    doc: {
+      content: 'block math block+',
+    },
+    heading: {
+      attrs: {
+        level: {
+          default: '1',
+        },
+        align: {
+          default: 'left',
+        },
+      },
+    },
+    pargraph: {
+      group: 'block',
+      content: 'text*',
+      parseDOM: [{ tag: 'p' }],
+      toDOM() {
+        const p = document.createElement('p')
+        return {
+          dom: p,
+          contentDOM: p,
+        }
+      },
+    },
+    text: {},
+  },
+  marks: {
+    bold: {
+      parseDOM: [{ tag: 'b' }],
+      toDOM() {
+        const b = document.createElement('b')
+        return {
+          dom: b,
+          contentDOM: b,
+        }
+      },
+    },
+  },
+})
+
+export const boldCommand = toggleMark(mySchema.marks.bold)
+
+const initState = EditorState.create({
+  schema: mySchema,
+  plugins: [
+    keymap(baseKeymap),
+    history(),
+    keymap({
+      'Mod-z': undo,
+      'Shift-Mod-z': redo,
+      'Mod-b': boldCommand,
+    }),
+  ],
+})
